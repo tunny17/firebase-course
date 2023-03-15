f3fdaeaf63aa9288bef8c4e3becd704009d89127
tunny17 <tvnji17@gmail.com>
2023-03-15 13:33:57 +0100

    upload function

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import { Auth } from './components/Auth';
 import { db, auth, storage } from './config/firebase';
 // this function is used to get a bunch of data collections from the database
 import { getDocs, collection, addDoc, deleteDoc, updateDoc, doc } from 'firebase/firestore';
+import { ref, uploadBytes } from 'firebase/storage';
+import { async } from '@firebase/util';
 
 
 const App = () => {
@@ -57,8 +59,16 @@ const App = () => {
 
 
   // uploads file
-  const uploadFile = () => {
-    
+  const uploadFile = async () => {
+    if (!fileUpload) return;
+    // path of the file to be uploaded to
+    const filesFolderRef = ref(storage, `projectFiles/${fileUpload.name}`);
+    // uploads the selected file to the file path
+    try {
+      await uploadBytes(filesFolderRef, fileUpload);
+    } catch(err) {
+      console.error(err);
+    }
   }
 
 
